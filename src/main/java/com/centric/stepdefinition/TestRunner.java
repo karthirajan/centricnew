3ddd1e3548ec50cfddc3e575c5ad62fa3a63d7ba
karthirajan <kajekarthi@gmail.com>
2020-02-23 10:55:21 +0530

    task

---
+++ src/main/java/com/centric/stepdefinition/TestRunner.java
@@ -0,0 +1,26 @@
+package com.centric.stepdefinition;
+
+import java.io.File;
+
+
+import java.io.File;
+
+import org.joda.time.LocalDateTime;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src\\main\\resources\\Features\\NewSeason.feature",
+                 glue = "com.centric.stepdefinition",
+                 plugin = {"html:target","json:target/reports.json"})
+
+public class TestRunner {
+
+	
+}
